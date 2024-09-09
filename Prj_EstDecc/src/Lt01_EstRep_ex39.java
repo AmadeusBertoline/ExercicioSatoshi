e84a177729eafff001582469259f450e61bd21ae
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2024-09-09 19:13:06 -0300

    Novo exercício - ex39, tabuleiro de xadrez

---
+++ Prj_EstDecc/src/Lt01_EstRep_ex39.java
@@ -0,0 +1,32 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Lt01_EstRep_ex39 {
+
+    public static void main(String[] args){
+        
+        long res;
+        
+        res = Tabuleiro();
+        
+        System.out.println("A quantidade de grãos no tabuleiro é: " +res);
+    
+    }
+    
+    static long Tabuleiro(){
+    
+        int i; 
+        long qtd2 = 2;
+        
+        //o long só armazena até este valor
+        //precisaria utilizar o biginteger para solucionar
+        for(i = 1; i <= 60; i++){
+            
+            qtd2 = qtd2 * 2;
+            System.out.println(qtd2);
+        }
+        
+        return qtd2;
+    }
+    
+}
