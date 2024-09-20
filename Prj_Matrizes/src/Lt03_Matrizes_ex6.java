05ca9e99159ea6d64931fdb48ded9a366b099619
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2024-09-19 22:20:28 -0300

    Novo exercício de matriz - 7

--- Prj_Matrizes/src/Lt03_Matrizes_ex6.java
+++ Prj_Matrizes/src/Lt03_Matrizes_ex6.java
@@ -10,8 +10,39 @@ public class Lt03_Matrizes_ex6 {
         for(i = 0; i <= 19; i++){
             
             Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o valor para a posição:" +i+ " do vetor");
             vt20[i] = scanner.nextInt();
         
+        }
+        
+        BubbleSort(vt20);
+        
+    }
+    
+    static void BubbleSort(int[] vt20){
+        
+        int i, j, aux;
+        
+        for(i = 0; i <= 19; i++){
+            
+            for(j = 0; j <= 18; j++){
+                
+                if(vt20[j] > vt20[j+1]){
+                    
+                    aux = vt20[j];
+                    vt20[j] = vt20[j+1];
+                    vt20[j+1] = aux;
+                    
+                }
+                
+            }
+            
+        }
+        
+        for(i = 0; i <= 19; i++){
+            
+            System.out.println(vt20[i]);
+            
         }
         
     }
