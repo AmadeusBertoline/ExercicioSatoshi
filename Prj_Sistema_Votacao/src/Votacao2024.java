ccf9f2da0b468026b3116f358a0e5919165419e5
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2024-10-22 19:17:57 -0300

    Início do projeto de sistema de votação

---
+++ Prj_Sistema_Votacao/src/Votacao2024.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Votacao2024 {
+
+    public Votacao[] CadastraVotacao(Votacao[] votacao)throws IOException{
+    
+        int i;
+        String fileName = "ArquivoVotacao.txt";
+        
+        
+        
+        return votacao;
+        
+    }
+    
+}
